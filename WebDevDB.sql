5a38628030ad529094cb22ea4cbb4cd5dd84605d
AVA NARAG <2224512@slu.edu.ph>
2023-11-26 11:46:48 +0000

    Replace WebDevDB.sql

--- WebDevDB.sql
+++ WebDevDB.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `test`;
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: test
 -- ------------------------------------------------------
--- Server version	8.2.0
+-- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -40,6 +40,7 @@ CREATE TABLE `accounts` (
 
 LOCK TABLES `accounts` WRITE;
 /*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (0,'Justine','Saw','12345','Guest'),(1,'Axella','Tulauan','12345','Guest'),(2,'Anthonette','Binuluan','12345','Guest'),(3,'Charles','Almazan','12345','Guest'),(4,'Cristoff','Estrada','12345','Guest'),(2212173,'Sofia','Surro','12345','Student'),(2221097,'Kaizer','Gura','12345','Student'),(2223144,'Ezrha','Dangilan','12345','Student'),(2223451,'Leovide','Bato','12345','Student'),(2224512,'Ava','Narag','12345','Student'),(2226098,'Geoff','Dulnuan','12345','Student');
 /*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -353,4 +354,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-26 15:54:47
+-- Dump completed on 2023-11-26 19:43:10
