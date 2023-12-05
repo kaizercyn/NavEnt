54c815cc8faa2a08bbc3f10c63d753f1145e87af
2212173 <2212173@slu.edu.ph>
2023-12-05 11:13:03 +0800

    php

---
+++ php/login.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require('dbconnection.php');
+if (isset($_POST['username'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $st = $conn -> prepare("SELECT * FROM ACCOUNTS WHERE User_ID=? and Password=?");
+    $st-> bind_param('ss', $username, $password);
+    $st-> execute();
+    $result= $st->get_result();
+    if ($result->num_rows !=0){
+        // $row = $result->fetch_assoc();
+        // $firstName = $row['First_Name'];
+        $_SESSION['username'] = $username;
+        header('Location: ../index.php');
+        // echo "logged in as: $firstName";
+    }else{
+        echo "wrong login credentials";
+    }
+    $st->close();
+}
+?>
