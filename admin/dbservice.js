6ef4e87050b4f6ee6600ebdbc4d4fd73c670484b
2212173 <2212173@slu.edu.ph>
2023-12-13 23:59:11 +0800

    admin folder restruc

---
+++ admin/dbservice.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.message);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+class DbService {
+    static getDbServiceInstance() {
+        return instance ? instance : new DbService();
+    }
+}
