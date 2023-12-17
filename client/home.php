7222211fbef28a4166640881140ce53f05dbf4a0
jepjepbulugao <2226098@slu.edu.ph>
2023-12-18 04:47:58 +0800

    mhm

--- client/home.php
+++ client/home.php
@@ -52,45 +52,48 @@
                 require("php/dbconnection.php");
                 $public = 1;
                 $open = 1;
-                $st = $conn -> prepare("SELECT * FROM EVENTS WHERE isPublic=? and isOpen=?;");
-                $st-> bind_param('ii', $public, $open);
+                $live = 1;
+                $st = $conn -> prepare("SELECT * FROM EVENTS WHERE isPublic=? and isOpen=? and isLive=?;");
+                $st-> bind_param('iii', $public, $open, $live);
                 $st-> execute();
                 $result= $st->get_result();
                 if ($result->num_rows !=0){
-                    foreach($result as $row){
-                        $rows [] = $row;
-                           
-                    }  
+                  $counter = 0;
+                $events = $result->fetch_all(MYSQLI_ASSOC);
+                    foreach ($events as $moment){ 
+                      $name = $moment['Event_Name'];
+                      $tagline = $moment['Event_Tagline'];
+                      $name1 = $moment['Event_Name'];
+                      $tagline1 = $moment['Event_Tagline'];
+                      $name2 = $moment['Event_Name'];
+                      $tagline2 = $moment['Event_Tagline'];
+                      
                 }
-
-                $st -> close();
-                $result -> close();
-                
-
+             }
+             $st -> close();
+             $result -> close();
             ?>
-            <p>$row</p>
           <div class="carousel-item active ev">
             <img src="res/imgs/event1.jpeg" class="d-block w-100 e-img" alt="event1">
             <div class="carousel-caption top-0 mt-4 d-none d-md-block">
-                <h2 class="mt-5c fs-3 text-uppercase"></h2>
-                <h1 class="display-1 fw-bolder text-capitalize"><?php 
-                echo $rows["Event_Name"]?></h1>
+                <h2 class="mt-5c fs-3 text-uppercase"><?php echo $tagline;?></h2>
+                <h1 class="display-1 fw-bolder text-capitalize"><?php echo $name; ?></h1>
                 <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Read More</button>
               </div>
         </div>
           <div class="carousel-item ev">
             <img src="res/imgs/event2.jpeg" class="d-block w-100 e-img" alt="event2">
             <div class="carousel-caption top-0 mt-4 d-none d-md-block">
-                <h2 class="mt-5c fs-3 text-uppercase">Tagline</h2>
-                <h1 class="display-1 fw-bolder text-capitalize">Event Name</h1>
+                <h2 class="mt-5c fs-3 text-uppercase"><?php echo $tagline1;?></h2>
+                <h1 class="display-1 fw-bolder text-capitalize"><?php echo $name1;?></h1>
                 <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Read More</button>
               </div>
         </div>
           <div class="carousel-item ev">
             <img src="res/imgs/event2.jpeg" class="d-block w-100 e-img" alt="event3">
             <div class="carousel-caption top-0 mt-4 d-none d-md-block">
-                <h2 class="mt-5c fs-3 text-uppercase">Tagline</h2>
-                <h1 class="display-1 fw-bolder text-capitalize">Event Name</h1>
+                <h2 class="mt-5c fs-3 text-uppercase"><?php echo $tagline2;?></h2>
+                <h1 class="display-1 fw-bolder text-capitalize"><?php echo $name2;?></h1>
                 <button class="btn btn-primary px-4 py-2 fs-5 mt-5">Read More</button>
               </div>
         </div>
