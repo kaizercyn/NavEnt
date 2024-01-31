c8f7b56ff1e900f46fb3ed8eb2d8e65eb83e7709
jepjepbulugao <2226098@slu.edu.ph>
2024-02-01 00:53:53 +0800

    Fixed bookmarking and announce functionality on client side.

--- client/bookmarked_events.php
+++ client/bookmarked_events.php
@@ -1,16 +1,6 @@
 <?php
 session_start();
 require "../php/dbconnection.php";
-$bookmarkedUserID = $_SESSION['userId']; 
-$st = $conn ->prepare("SELECT Event_ID FROM REGISDETAILS WHERE User_ID=?");
-$st -> bind_param("s",$bookmarkedUserID);
-$st -> execute();
-$result = $st -> get_result();
-if($result -> num_rows != 0){
-  $bookmarkedEvents = $result->fetch_all(MYSQLI_ASSOC);
-}
-$st->close();
-$result->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -46,13 +36,13 @@ $result->close();
               </div>
             </button>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="client/account_details.php">Account Details</a></li>
-              <li><a class="dropdown-item" href="client/bookmarked_events.php">Bookmarks</a></li>
-              <li><a class="dropdown-item" href="client/event_history.php">Event History</a></li>
-              <li><a class="dropdown-item" href="client/pending_evaluation.php">Pending Evaluations</a></li>
+              <li><a class="dropdown-item" href="../client/account_details.php">Account Details</a></li>
+              <li><a class="dropdown-item" href="../client/bookmarked_events.php">Bookmarks</a></li>
+              <li><a class="dropdown-item" href="../client/event_history.php">Event History</a></li>
+              <li><a class="dropdown-item" href="../client/pending_evaluation.php">Pending Evaluations</a></li>
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">
-                <form action="logout.php" method="post"><button type="submit" class="dropdown-item" onclick="return confirm('Are you sure you want to logout?')">Logout</button></form></a></li>
+                <form action="..php/logout.php" method="post"><button type="submit" class="dropdown-item" onclick="return confirm('Are you sure you want to logout?')">Logout</button></form></a></li>
             </ul>
           </div>
         </div>
@@ -67,18 +57,29 @@ $result->close();
     </nav>
 
     <section id="packages" class="pt-3 pb-3 custom-left-padding">
-      <h2 class="text-left my-3">Bookmarked Events<h2>
-    </section>
+      <h2 class="text-left my-3">Bookmarked Events</h2>
     <?php
-    while($bookmarkedEvents != 0){
-      $eventBookmarked = array_shift($bookmarkedEvents);
-      $st = $conn -> prepare("SELECT * FROM EVENTS WHERE Event_ID=?");
-      $st -> bind_param('i',$eventBookmarked);
-      $st -> execute();
-      $result = $st -> get_result();
-      if($result -> num_rows != 0){
+    $bookmarkedUserID = $_SESSION['userId']; 
+    $st = $conn ->prepare("SELECT Event_ID FROM bookmarks WHERE User_ID=?");
+    $st -> bind_param("i",$bookmarkedUserID);
+    $st -> execute();
+    $result = $st -> get_result();
+    if($result -> num_rows != 0){
+      $bookmarkedEvents = $result->fetch_all(MYSQLI_ASSOC);
+    }
+    $st->close();
+    $result->close();
+    $bookmarkedEventsCount = count($bookmarkedEvents);
+     for($i = 0; $i < $bookmarkedEventsCount; $i++){
+        $EventItself = array_shift($bookmarkedEvents[$i]);  
+        $st = $conn -> prepare("SELECT * FROM EVENTS WHERE Event_ID=?");
+        $st -> bind_param('s',$EventItself);
+        $st -> execute();
+        $result = $st -> get_result();
+        if($result -> num_rows != 0){
         $bmEvents = $result -> fetch_all(MYSQLI_ASSOC);
-      
+        $eventBook = array_shift($bmEvents);
+   
 
     ?>
     <div id="latestEventsSlider" class="carousel slide" data-bs-ride="false">
@@ -87,24 +88,30 @@ $result->close();
             <div class="container">
                 <div class="col-lg">
                   <div class="card latest-event w-100">
-                    <img src="https://images.unsplash.com/photo-1560439514-07abbb294a86?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img" alt="event4">
+                   <!--- <img src="https://images.unsplash.com/photo-1560439514-07abbb294a86?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img" alt="event4"> -->
                     <div class="card-img-overlay">
-                      <?php $eventBm = array_shift($bmEvents); ?>
-                        <h5 class="card-title"><?php echo $eventBm['Event_Name']; ?></h5>
-                        <p class="card-text"><?php echo $eventBm['Event_Description']; ?></p>
-                        <p class="card-text"><small><?php echo $eventBm['Event_StartDate']; ?></small></p>
-                        <input type="hidden" name="event" value="<?php echo $eventBm['Event_ID'];  ?>">
-                        <button type="submit" class="btn btn-primary" name="readMore">Read More</button>
+                      <h5 class="card-title"><?php echo $eventBook['Event_Name']; ?></h5>
+                      <p class="card-text"><?php echo $eventBook['Event_Description']; ?></p>
+                      <p class="card-text"><small><?php echo $eventBook['Event_StartDate']; ?></small></p>
+                      <form action="../client/event_details.php" method="POST">
+                      <input type="hidden" name="event" value="<?php echo $eventBook['Event_ID'];  ?>">
+                      <button type="submit" class="btn btn-primary" name="readMore">Read More</button>
+                      </form>
                     </div>
                   </div>
                 </div>
             </div>
         </div>
     </div>
+  </div>
     <?php
+                      
       }
+      $st -> close();
+      $result -> close();
     }
     ?>
+    </section>
     <footer>
         <div class="footer-bottom">
             <div class="logo">
